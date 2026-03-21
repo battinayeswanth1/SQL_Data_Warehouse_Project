90e391e2d3954f43f55cf734d44af485b661742a
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-03-21 13:36:36 +0000

    Update silver_layer_load.sql

--- Scripts/Silver/silver_layer_load.sql
+++ Scripts/Silver/silver_layer_load.sql
@@ -68,3 +68,48 @@ insert into dw_silver.crm_prd_info(
     lead(prd_start_dt) over(partition by prd_key order by prd_start_dt) - interval 1 day as prd_end_dt
   from crm_prd_info;
 
+
+truncate table dw_silver.crm_sales_details;
+insert into dw_silver.crm_sales_details(
+	sls_ord_num, 
+    sls_prd_key, 
+    sls_cst_id, 
+    sls_order_dt, 
+    sls_ship_dt, 
+    sls_due_dt, 
+    sls_sales, 
+    sls_quantity, 
+    sls_price
+)
+select 
+	sls_ord_num, 
+    sls_prd_key, 
+    sls_cst_id,
+	case
+		when sls_order_dt <= 0 or length(sls_order_dt) != 8 then null
+        else str_to_date(sls_order_dt , '%Y%m%d') 
+        end sls_order_dt,
+	case
+		when sls_ship_dt <= 0 or length(sls_ship_dt) != 8 then null
+        else str_to_date(sls_ship_dt , '%Y%m%d') 
+        end sls_ship_dt,
+	case
+		when sls_due_dt <= 0 or length(sls_due_dt) != 8 then null
+        else str_to_date(sls_due_dt , '%Y%m%d') 
+        end sls_due_dt,
+	case 
+		when (sls_sales <=0 or sls_sales is null or sls_sales != sls_quantity * abs(sls_price)) and (sls_quantity >0 and sls_price >0)
+        then sls_quantity * abs(sls_price)
+        else sls_sales
+        end sls_sales,
+	sls_quantity,
+	case
+		when sls_price <=0 then round(sls_sales/sls_quantity)
+        else sls_price
+        end sls_price
+from crm_sales_details
+
+
+
+
+
